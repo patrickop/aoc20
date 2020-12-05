69e07c53f8bf654fa4fad9ce9e988df17e409ebc
Patrick <patrick.opgenoorth@gmail.com>
2020-12-05 13:55:51 +0000

    final day1

--- app/Day1.hs
+++ app/Day1.hs
@@ -1,6 +1,16 @@
 module Main where
 
-import Lib
+import Parsing
+import Analysing
 
 main :: IO ()
-main = someFunc
+main = do
+        numbers <- parseIntsFile "data/day1/input.txt"
+        let t1 = duos numbers
+        let t2 = triples numbers
+        let combinationT1 = findCombosThatAddTo t1 2020
+        let combinationT2 = findCombosThatAddTo t2 2020
+        let resultT1 = multiply' combinationT1
+        let resultT2 = multiply' combinationT2
+        putStrLn ("duos: " ++ ( show resultT1 ))
+        putStrLn ("triples: " ++ ( show resultT2 ))
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,9 @@
 import Test.HUnit
 import Parsing
+import Analysing
+import Data.Sort
 
-test1 :: Test
-test1 = TestCase (assertEqual "addone adds one" 4 (addone 3))
-
+-- Test parsing
 testParseInts :: Test
 testParseInts = TestCase (
             do 
@@ -14,10 +14,36 @@ testParseInts = TestCase (
                 result 
             )
 
+-- Test analysing
+testDuos :: Test
+testDuos = TestCase (
+              assertEqual
+                "finds all duos"
+                (sort [[1,2], [1,3], [1,4], [2,3], [2,4], [3,4]])
+                (sort (duos[1, 2, 3, 4]))
+            )
+testTriples :: Test
+testTriples = TestCase (
+              assertEqual
+                "finds all duos"
+                (sort [[1,2,3], [1,2,4], [2,3,4], [1,3,4]])
+                (sort (triples[1, 2, 3, 4]))
+            )
+
+testFindNumbersThatAddTo :: Test
+testFindNumbersThatAddTo = TestCase (
+              assertEqual
+                "finds number pair that adds to 6"
+                (Just [2,4])
+                (findCombosThatAddTo [[1,2], [1,3], [1,4], [2,3], [2,4], [3,4]] 6)
+            )
+
 tests :: Test
 tests = TestList [
-          TestLabel "test1" test1,
-          TestLabel "testParseInts" testParseInts
+          TestLabel "testParseInts" testParseInts,
+          TestLabel "testDuos" testDuos,
+          TestLabel "testTriples" testTriples,
+          TestLabel "testFindNumbersThatAddTo" testFindNumbersThatAddTo
         ]
 
 main :: IO Counts
